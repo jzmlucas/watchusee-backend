a0f45894040a79d688455d36cb0cf2b936d075c5
Lucas Joly <lucasjsmorais1@hotmail.com>
2026-09-08 00:52:13 -0300

    aumento de itens na watchlist

--- src/main/java/br/com/watchusee/watchusee/watchlist/api/UserWatchlistController.java
+++ src/main/java/br/com/watchusee/watchusee/watchlist/api/UserWatchlistController.java
@@ -77,7 +77,7 @@ public class UserWatchlistController {
             @Min(0)
             int page,
 
-            @RequestParam(defaultValue = "20")
+            @RequestParam(defaultValue = "100")
             @Min(1)
             @Max(100)
             int size
